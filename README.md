f0309403ee27df42c31e13621dedcae28f22ac0b
Gerard Roche <gerardroche@users.noreply.github.com>
2023-05-19 20:17:00 +0100

    Initial version

---
+++ README.md
@@ -0,0 +1,9 @@
+# NeoVintageousLineNumbers
+
+A [NeoVintageous](https://github.com/NeoVintageous/NeoVintageous) plugin to auto disable relative line numbers in Insert mode.
+
+## Installation
+
+**Pending availability in Package Control.**
+
+Install NeoVintageousLineNumbers via [Package Control](https://packagecontrol.io/packages/NeoVintageousLineNumbers).
---
+++ plugin.py
@@ -0,0 +1,30 @@
+import sublime
+import sublime_plugin
+
+# Relative line numbers is supported >= 4074.
+
+if int(sublime.version()) >= 4050:
+    def _is_normal_view(view) -> bool:
+        return view and view.element() is None
+else:
+    def _is_normal_view(view) -> bool:
+        if not view:
+            return False
+
+        settings = view.settings()
+
+        return settings and not settings.get('is_widget', False)
+
+
+def _is_command_mode(view) -> bool:
+    return _is_normal_view(view) and view.settings().get('command_mode')
+
+
+class LineNumbersEvents(sublime_plugin.EventListener):
+
+    def on_post_text_command(self, view, command_name, args):
+        if view.settings().get('line_numbers'):
+            if _is_command_mode(view):
+                view.settings().set('relative_line_numbers', True)
+            else:
+                view.settings().set('relative_line_numbers', False)
